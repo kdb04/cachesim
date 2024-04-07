49bd86e4ce914c0a145ab515a5b02ee857cf4a91
kdb04 <nikhil.srivatsa1@gmail.com>
2024-04-07 19:58:05 +0530

    main

--- cache_sim.py
+++ cache_sim.py
@@ -7,6 +7,20 @@ class cache_sim:
         self.hits = 0
         self.miss = 0
         self.evictions = 0
-        self.cache = -1*self.blocks  #initially empty cache
+        self.cache = -1*self.blocks  #initially empty cache(-1)
 
 
+def main():
+    cache_size = int(input("Enter cache size(bytes)"))
+    block_size = int(input("Enter block size(bytes)"))
+    map_tech = input("Enter mapping technique")
+    user_input = ("Enter comma separated values").split(",")
+
+    cache = cache_sim(cache_size, block_size, map_tech)
+
+    print("Total Hits:", cache.hits)
+    print("Total Misses:", cache.miss)
+    print("Total Evictions:", cache.evictions)
+
+if __name__ == "__main__":
+    main()
